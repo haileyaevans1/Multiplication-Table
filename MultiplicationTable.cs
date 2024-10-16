55c4e7ffd8bcb7d8934f56338ec3ad82873cfb15
haileyaevans1 <haileyaevans1@gmail.com>
2024-10-16 15:16:34 -0500

    capitalized Console and fixed logic

--- MultiplicationTable.cs
+++ MultiplicationTable.cs
@@ -1,19 +1,31 @@
-using System
-public class MultiplicationTable
+ConsoleApplication.Run();
+
+public class ConsoleApplication
 {
-    static void Main() args
+    public static void Run()
     {
-        console.WriteLine("Enter starting value for rows: ");
-        int startingRow = console.ReadLine();
-        console.WriteLine("Enter ending value for rows: ");
-        int endingRow = console.ReadLine();
-        console.WriteLine("Enter starting value for columns: ");
-        int startingColumn = console.ReadLine();
-        console.WriteLine("Enter ending value for columns: ");
-        int endingColumn = console.ReadLine();
+        Console.Write("Enter starting value for rows: ");
+        int startingRow = int.Parse(Console.ReadLine());
+        Console.Write("Enter ending value for rows: ");
+        int endingRow = int.Parse(Console.ReadLine());
+        Console.Write("Enter starting value for columns: ");
+        int startingColumn = int.Parse(Console.ReadLine());
+        Console.Write("Enter ending value for columns: ");
+        int endingColumn = int.Parse(Console.ReadLine());
+        Console.WriteLine();
+
+        Console.Write("\t");
+        for (int j = startingColumn; j <= endingColumn; j++)
+        {
+            Console.Write($"{j}\t");
+        }
+        Console.WriteLine();
+
+        Console.Write("============================================================\n");
 
         for (int i = startingRow; i <= endingRow; i++)
         {
+            Console.Write($"{i}\t");
             for (int j = startingColumn; j <= endingColumn; j++)
             {
                 Console.Write($"{i * j}\t");
